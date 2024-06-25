d8b03b0dd46a92169fcf142d77ebf6d0a5444c7e
hasancan.bayram <hasancan.bayram@tigahealth.com>
2024-06-25 16:39:23 +0300

    compositional layout

--- RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -10,11 +10,26 @@ import Foundation
 final class RMCharacterDetailViewViewModel {
     private let character: RMCharacter
     
+    enum SectionType: CaseIterable {
+        case photo
+        case information
+        case episodes
+    }
+    
+    public let sections = SectionType.allCases
+    
+    // MARK: - Init
+    
     init(character: RMCharacter) {
         self.character = character
+    }
+    
+    private var requestUrl: URL? {
+        return URL(string: character.url)
     }
     
     public var title: String {
         return character.name.uppercased()
     }
+    
 }
